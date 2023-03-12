209c05915243c92621d4c67d8a69d89acc9b5365
TypicalDisson <127669242+TypicalDisson@users.noreply.github.com>
2023-03-12 18:51:34 +0500

    Update Pingpong.py
    Добавлен классы GameObject и PLayer

--- Pingpong.py
+++ Pingpong.py
@@ -1,11 +1,45 @@
 from pygame import *
 
+
+
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width,win_height))
 
 clock = time.Clock()
 FPS = 60
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,size_x,size_y,player_speed):
+        sprite.Sprite.__init__(self)
+
+        self.image = transform.scale(image.load(player_image),(size_x,size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.player_x
+        self.rect.y = self.player_y
+
+    def reset(self):
+        window.blit(self.image,(self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
 
 game = True 
 while game:
@@ -14,6 +48,7 @@ while game:
             game = False
 
     window.fill((147, 112, 219))
+
 
     display.update()
     clock.tick(FPS)
